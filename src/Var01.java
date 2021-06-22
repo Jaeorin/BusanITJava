ccc10b9cc8eb89705d3476b80ef48cee4a157b64
jaeorin <47616192+jaeorin@users.noreply.github.com>
2021-06-22 16:09:02 +0900

    누락된 파일 추가

---
+++ src/Var01.java
@@ -0,0 +1,10 @@
+public class Var01 {
+    public static void main(String[] args) {
+        System.out.println("안녕 자바"); // 문자열 String
+        System.out.println(7); // 정수 Int
+        System.out.println(9.8); // 실수 float, double
+        System.out.println('가'); // 하나의 문자 char
+        System.out.println(true); // boolean 참
+        System.out.println(false); // boolean 거짓
+    }
+}
